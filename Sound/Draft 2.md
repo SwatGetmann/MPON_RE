9b94276d1662072ee6a65a6ed4dfa2ebf016e5c3
SwatGetmann <swatgetmann@gmail.com>
2020-12-30 00:44:41 +0300

    extract nsGlobal

--- Sound/Draft 2.md
+++ Sound/Draft 2.md
@@ -15,7 +15,7 @@
 * `%GAMEDIR%\common\Sound\NGGLOBAL_400.WAD`
 * `%GAMEDIR%\common\Sound\NGGLOBAL_400.IDX`
 
-* `%GAMEDIR%\sound\NSGLOBAL_400.WAD`
+* `%GAMEDIR%\sound\NSGLOBAL_400.WAD` - In-Game Cinematics / Music / SFX + Headers
 
 * `%GAMEDIR%\sound\NSD_EN_158.WAD`
 * `%GAMEDIR%\sound\NSF_EN_158.WAD` - Streams for Film / Cinematics
@@ -23,7 +23,7 @@
 * `%GAMEDIR%\sound\NS_EN_400.WAD`
 * `%GAMEDIR%\sound\NL_EN_400.WAD`
 
-* `%GAMEDIR%\sound\NP3_PSTL_*.WAD`
+* `%GAMEDIR%\sound\NP3_PSTL_*.WAD` - relative to RIM/NIMs
 
 # Knowledge
 
@@ -41,7 +41,37 @@
 * `\sound\NSF_EN_158.WAD` - Streams for Film / Cinematics. They must be related to... files w/ Movies themselves.
 
 * Some strange gap in `NSGLOBAL_400.WAD`, starting off last audio block @ `103954704` (`0x6323910`), Until `0x6800000`
+* Some strange gap in `NSGLOBAL_400.WAD`, starting off last audio block @ `209004920` (`0xC752978`), Until `0xCC00000`
+* Gap in `NSGLOBAL_400.WAD`, @ `0x12C00000` - Until `0x13000000`
+* Some strange audio in `NSGLOBAL_400.WAD`, audio block @ `313949052` (`0x12B67B7C`). It repeats 3 times, thoough 2 time NOT BROKEN: @ `0x13000000`, @ `0x23460000`
+* Audio issues in `NSGLOBAL_400.WAD`, @ `419262560` (`0x18FD7060`) - start of header @ `0x19000000` - `0x19400000`
+* Audio issues in `NSGLOBAL_400.WAD`, @ `522452460` (`0x1F23FDEC`) - start of header @ `0x1F400000` - `0x1F800000`
+
 * `NSGLOBAL_400.WAD` - some weird HEADER info (+ NAMES OF TRACKS!):
-* * @ `0x6660000` - `0x6660260` 
-* * @ `0x6658000` - `0x6658060`
-* * @ `0x6400000` - `0x6654de0`
+
+* * @ `0x6400000` - `0x6654de0` (`0x0`,         + 2444768 bytes, `0x254DE0`)
+* * @ `0x6658000` - `0x6658060` (`0x258000`,    +, `0x60`)
+* * @ `0x6660000` - `0x6660260` (`0x260000`,    +, `0x260`)
+
+* * @ `0xC800000` - `0xCA54DE0`
+* * @ `0xCA58000` - `0xCA58060`
+* * @ `0xCA60000` - `0xCA60260`
+
+* * @ `0x12C00000` - `0x12E54DE0`
+* * @ `0x12E58000` - `0x12E58060`
+* * @ `0x12E60000` - `0x12E60260`
+
+* * @ `0x19000000` - `0x19254DE0`
+* * @ `0x19258000` - `0x19258060`
+* * @ `0x19260000` - `0x19260260`
+
+* * @ `0x1F400000` - `0x1F654DE0`
+* * @ `0x1F658000` - `0x1F658060`
+* * @ `0x1F660000` - `0x1F660260`
+
+* (?) NSGLOBAL Headers (0x400000) may refer to the exact order STREAM data is stored, not labeled in the beginning header... \ 
+That is yet to be checked.
+
+* `NP3_PSTL` files refer to Gap-Header in `NSGLOBAL_400.WAD`
+* Good examples: `Katana`, `MX_B2_` (`I_Say.WAD`). Gap-Headers in NSGlobal have DIFFERENT values in the first block - or at least the names are different ... \
+And these indexes are either the previous or following to the ones from `NP3_PSTL`...
--- Sound/Global/nsGlobal_extractor.rb
+++ Sound/Global/nsGlobal_extractor.rb
@@ -28,6 +28,10 @@ def save_wav(path, size, channels, samplerate, stream)
   sf.close()
 end
 
+def handle_broken_block()
+  # TBD
+end
+
 # ruby 'F:\22 Code Playground\Matrix PON Reverse Engineering\Sound\Global\nsGlobal_extractor.rb' 'g:\Games\The Matrix - Path of Neo' 'f:\22 Code Playground\Matrix PON Reverse Engineering\Sound\Global\ExtractNSGlobal'
 
 game_dir_path = ARGV[0]
@@ -50,13 +54,12 @@ global_idx = 0
 wad_rf = File.open(wad_fp, 'rb+')
 _, offset = wad_rf.read(8).unpack('LL')
 while wad_rf.pos - 8 < offset
-  # sample
   b_sample = wad_rf.read(4).unpack('L').first
   while b_sample == 4294967295
     b_sample = wad_rf.read(4).unpack('L').first
     next
   end
-  if b_sample >= 0
+  if b_sample >= 0 && wad_rf.pos-4 < offset + 8
     header_addr = wad_rf.pos-4
     size, channels, samplerate = wad_rf.read(8).unpack('LSS')
     # "HEADER: [<inner_header_idx> | <global_idx>] : <address> , <audio_signature>"
@@ -71,7 +74,6 @@ while wad_rf.pos - 8 < offset
       found: false
     }
     global_idx += 1
-    # binding.pry if global_idx % 500 == 0 and global_idx > 0
   else 
     break;
   end
@@ -82,22 +84,118 @@ puts "="*100
 while global_idx > 0
   b_sample = wad_rf.read(4).unpack('L').first
   next if b_sample == 0
+  # binding.pry if wad_rf.pos >= 0xCC00000
+  # binding.pry if global_idx == 1
   if headers_array.select{|x| !x[:found]}.map{|h| h[:size]}.include?(b_sample)
     stream_addr = wad_rf.pos-4
-    if stream_addr == 103954704
-        puts "Have to exit due to fucked up shit"
-        exit!
-    end
     header = headers_array.select{|x| !x[:found]}.find{|h| h[:size] == b_sample}
     _channels, _samplerate = wad_rf.read(4).unpack('SS')
     puts "STREAM: [#{header[:header_idx]} | #{header[:global_idx]}] : #{stream_addr} , {#{header[:size]}, #{header[:channels]}, #{header[:samplerate]}}. Left to find: #{global_idx - 1}"
+
+    # CHANGE to HANLDE BROKEN BLOCK
+
+    if stream_addr == 103954704
+      puts "Fucked up DATA CHUNK shit"
+      length_to_read = 0x6400000 - wad_rf.pos
+
+      stream = wad_rf.read(length_to_read)
+      header[:found] = false
+      save_path = File.join(extract_path, "NSGLOBAL_400__#{'%04i' % header[:global_idx]}_#{header[:header_idx]}_false_stream.wav")
+      save_wav(save_path, length_to_read, header[:channels], header[:samplerate], stream)
+      puts "Stream read & saved (though FALSE). Position: #{wad_rf.pos}"
+      # global_idx -= 1
+
+      puts "!!! MOVE TO A VALID NEW ADDRESSS!"
+      new_addr = 0x6800000
+      wad_rf.seek(new_addr, IO::SEEK_SET)
+      next
+    end
+
+    if stream_addr == 209004920
+      puts "FUCKED UP block x2"
+
+      # STREAM: [5769 | 3035] : 209004920 , {1679832, 2, 22000}. Left to find: 2726
+      # Stream read & saved. Position: 210684760
+
+      length_to_read = 0xC800000 - wad_rf.pos
+
+      stream = wad_rf.read(length_to_read)
+      header[:found] = false
+      save_path = File.join(extract_path, "NSGLOBAL_400__#{'%04i' % header[:global_idx]}_#{header[:header_idx]}_false_stream.wav")
+      save_wav(save_path, length_to_read, header[:channels], header[:samplerate], stream)
+      puts "Stream read & saved (though FALSE). Position: #{wad_rf.pos}"
+      # global_idx -= 1
+
+      puts "!!! MOVE TO A VALID NEW ADDRESSS!"
+      new_addr = 0xCC00000
+      wad_rf.seek(new_addr, IO::SEEK_SET)
+      next
+    end
+
+    if stream_addr == 313949052
+      puts "Strange block x3"
+
+      length_to_read = 0x12C00000 - wad_rf.pos
+
+      stream = wad_rf.read(length_to_read)
+      header[:found] = false
+      save_path = File.join(extract_path, "NSGLOBAL_400__#{'%04i' % header[:global_idx]}_#{header[:header_idx]}_false_stream.wav")
+      save_wav(save_path, length_to_read, header[:channels], header[:samplerate], stream)
+      puts "Stream read & saved (though FALSE). Position: #{wad_rf.pos}"
+      # global_idx -= 1
+
+      puts "!!! MOVE TO A VALID NEW ADDRESSS!"
+      new_addr = 0x13000000
+      wad_rf.seek(new_addr, IO::SEEK_SET)
+      next
+    end
+
+    if stream_addr == 419262560
+      puts "Strange block x4"
+
+      length_to_read = 0x19000000 - wad_rf.pos
+
+      stream = wad_rf.read(length_to_read)
+      header[:found] = false
+      save_path = File.join(extract_path, "NSGLOBAL_400__#{'%04i' % header[:global_idx]}_#{header[:header_idx]}_false_stream.wav")
+      save_wav(save_path, length_to_read, header[:channels], header[:samplerate], stream)
+      puts "Stream read & saved (though FALSE). Position: #{wad_rf.pos}"
+      # global_idx -= 1
+
+      puts "!!! MOVE TO A VALID NEW ADDRESSS!"
+      new_addr = 0x19400000
+      wad_rf.seek(new_addr, IO::SEEK_SET)
+      next
+    end
+
+    if stream_addr == 522452460
+      puts "Strange block x5"
+
+      length_to_read = 0x1F400000 - wad_rf.pos
+
+      stream = wad_rf.read(length_to_read)
+      header[:found] = false
+      save_path = File.join(extract_path, "NSGLOBAL_400__#{'%04i' % header[:global_idx]}_#{header[:header_idx]}_false_stream.wav")
+      save_wav(save_path, length_to_read, header[:channels], header[:samplerate], stream)
+      puts "Stream read & saved (though FALSE). Position: #{wad_rf.pos}"
+      # global_idx -= 1
+
+      puts "!!! MOVE TO A VALID NEW ADDRESSS!"
+      new_addr = 0x1F800000
+      wad_rf.seek(new_addr, IO::SEEK_SET)
+      next
+    end
+
     stream = wad_rf.read(header[:size])
     header[:found] = true
     save_path = File.join(extract_path, "NSGLOBAL_400__#{'%04i' % header[:global_idx]}_#{header[:header_idx]}.wav")
     save_wav(save_path, header[:size], header[:channels], header[:samplerate], stream)
     puts "Stream read & saved. Position: #{wad_rf.pos}"
+
     global_idx -= 1
   end
 end
 
 wad_rf.close()
+
+puts "FINISHED EXTRACTING!"
