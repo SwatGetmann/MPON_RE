d6ff5188fa771c9a7ed06708ef7e69884ec65d7b
SwatGetmann <swatgetmann@gmail.com>
2021-01-03 22:22:32 +0300

    refactoring of extractor

--- Sound/Global/nsGlobal_extractor_NS_EN_400_based.rb
+++ Sound/Global/nsGlobal_extractor_NS_EN_400_based.rb
@@ -60,9 +60,11 @@ module MPONExtract
   end
 
 
-  def self.read_merge_nsglobal_400_headers(ns_en_offsets, wad_fp)
-    puts "Reading: #{wad_fp} [NSGLOBAL_400 HEADERS] ..."
-
+  def self.read_merge_wad_fp_headers(ns_en_offsets, wad_fp)
+    wad_basename = File.basename(wad_fp)
+    wad_name = wad_basename.gsub(/\.WAD$/, '')
+    
+    puts "Reading: #{wad_fp} [#{wad_name} HEADERS] ..."
     global_idx = 0
     
     wad_rf = File.open(wad_fp, 'rb+')
@@ -88,7 +90,7 @@ module MPONExtract
             size: size,
             channels: channels,
             samplerate: samplerate,
-            source: 'NSGLOBAL_400.WAD',
+            source: wad_basename,
             found: false
           })
         else
@@ -105,96 +107,36 @@ module MPONExtract
     puts "="*100
   end
 
-  def self.read_merge_nsf_en_158_headers(ns_en_offsets, wad_fp)
-    puts "Reading: #{wad_fp} [NSF_EN_158 HEADERS] ..."
 
-    global_idx = 0
-    
+  def self.extract_streams_wad_fp(ns_en_offsets, wad_fp, adb_data, extract_path, msg)
+    wad_basename = File.basename(wad_fp)
+    wad_name = wad_basename.gsub(/\.WAD$/, '')
+
+    puts "START EXTRACTING [#{wad_basename}] #{msg} ..."
     wad_rf = File.open(wad_fp, 'rb+')
-    _, offset = wad_rf.read(8).unpack('LL')
-    while wad_rf.pos - 8 < offset
-      b_sample = wad_rf.read(4).unpack('L').first
-      while b_sample == OVERFLOW_VALUE
-        b_sample = wad_rf.read(4).unpack('L').first
-        next
+    nsglobal_headers = ns_en_offsets.select{|r| r.keys.include?(:source) && r[:source] == wad_basename}
+    puts "TOTAL ADB: #{nsglobal_headers.size}"
+    nsglobal_headers.each do |nsg_header|
+      puts "[#{nsg_header[:source]}] EXTRACTING: #{nsg_header[:id]} @ #{nsg_header[:offset]} {#{nsg_header[:size]} , #{nsg_header[:channels]} , #{nsg_header[:samplerate]}}"
+      adb_block = adb_data.find{|r| r[:adb_index] == nsg_header[:id]}
+      if adb_block
+        puts "NAME: #{adb_block[:header_name]}"
+
+        wad_rf.seek(nsg_header[:offset]+8, IO::SEEK_SET)
+        stream = wad_rf.read(nsg_header[:size])
+        nsg_header[:found] = true
+
+        save_path = File.join(extract_path, "#{wad_name}__#{'%05i' % nsg_header[:id]}__#{adb_block[:header_name]}.wav")
+        save_wav(save_path, nsg_header[:size], nsg_header[:channels], nsg_header[:samplerate], stream)
+        puts "Stream read & saved. Position: #{wad_rf.pos}"
+      else
+        puts "ADB BLOCK NOT FOUND!"
+        binding.pry
       end
-      if b_sample >= 0 && wad_rf.pos-4 < offset + 8
-        header_addr = wad_rf.pos-4
-        size, channels, samplerate = wad_rf.read(8).unpack('LSS')
-        # "HEADER: [<inner_header_idx> | <global_idx>] : <address> , <audio_signature>"
-        puts "HEADER: [#{b_sample} | #{global_idx}] : #{header_addr} , {#{size} , #{channels}, #{samplerate/1000.0}k}"
-    
-        offset_hash = ns_en_offsets.find{|h| h[:id] == b_sample}
-        if offset_hash
-          offset_hash.merge!({ 
-            header_idx: b_sample,
-            global_idx: global_idx,
-            header_addr: header_addr,
-            size: size,
-            channels: channels,
-            samplerate: samplerate,
-            source: 'NSF_EN_158.WAD',
-            found: false
-          })
-        else
-          puts "NOT FOUND OFFSET HASH FOR ID: #{b_sample}"
-          binding.pry
-        end
-        global_idx += 1
-      else 
-        break
-      end
-      
     end
     wad_rf.close()
-    puts "="*100
+    puts "FINISHED EXTRACTING [NSF_EN_158.WAD] !"
   end
-
-  def self.read_merge_nsd_en_158_headers(ns_en_offsets, wad_fp)
-    puts "Reading: #{wad_fp} [NSD_EN_158 HEADERS] ..."
-
-    global_idx = 0
-    
-    wad_rf = File.open(wad_fp, 'rb+')
-    _, offset = wad_rf.read(8).unpack('LL')
-    while wad_rf.pos - 8 < offset
-      b_sample = wad_rf.read(4).unpack('L').first
-      while b_sample == OVERFLOW_VALUE
-        b_sample = wad_rf.read(4).unpack('L').first
-        next
-      end
-      if b_sample >= 0 && wad_rf.pos-4 < offset + 8
-        header_addr = wad_rf.pos-4
-        size, channels, samplerate = wad_rf.read(8).unpack('LSS')
-        # "HEADER: [<inner_header_idx> | <global_idx>] : <address> , <audio_signature>"
-        puts "HEADER: [#{b_sample} | #{global_idx}] : #{header_addr} , {#{size} , #{channels}, #{samplerate/1000.0}k}"
-    
-        offset_hash = ns_en_offsets.find{|h| h[:id] == b_sample}
-        if offset_hash
-          offset_hash.merge!({ 
-            header_idx: b_sample,
-            global_idx: global_idx,
-            header_addr: header_addr,
-            size: size,
-            channels: channels,
-            samplerate: samplerate,
-            source: 'NSD_EN_158.WAD',
-            found: false
-          })
-        else
-          puts "NOT FOUND OFFSET HASH FOR ID: #{b_sample}"
-          binding.pry
-        end
-        global_idx += 1
-      else 
-        break
-      end
-      
-    end
-    wad_rf.close()
-    puts "="*100
-  end
-
 
   def self.extract_streams_nsglobal_400(ns_en_offsets, wad_fp, adb_data, extract_path)
     puts "START EXTRACTING [NSGLOBAL_400.WAD] - SFX + Additional Music ..."
@@ -299,11 +241,11 @@ f_wad_fp = File.join(game_dir_path,'sound', 'NSF_EN_158.WAD')
 ns_en_fp = File.join(game_dir_path,'sound', 'NS_EN_400.IDX')
 
 ns_en_offsets = MPONExtract::read_ns_es_400_offsets(ns_en_fp)
-MPONExtract::read_merge_nsglobal_400_headers(ns_en_offsets, wad_fp)
-MPONExtract::read_merge_nsf_en_158_headers(ns_en_offsets, f_wad_fp)
-MPONExtract::read_merge_nsd_en_158_headers(ns_en_offsets, d_wad_fp)
 
+MPONExtract::read_merge_wad_fp_headers(ns_en_offsets, wad_fp)
+MPONExtract::read_merge_wad_fp_headers(ns_en_offsets, d_wad_fp)
+MPONExtract::read_merge_wad_fp_headers(ns_en_offsets, f_wad_fp)
 
-# MPONExtract::extract_streams_nsglobal_400(ns_en_offsets, wad_fp, adb_data, extract_path)
-# MPONExtract::extract_streams_nsf_en_158(ns_en_offsets, f_wad_fp, adb_data, extract_path)
-MPONExtract::extract_streams_nsd_en_158(ns_en_offsets, d_wad_fp, adb_data, extract_path)
+MPONExtract::extract_streams_wad_fp(ns_en_offsets, wad_fp, adb_data, extract_path, 'SFX + Additional Music')
+MPONExtract::extract_streams_wad_fp(ns_en_offsets, f_wad_fp, adb_data, extract_path, 'Movies Mixes')
+MPONExtract::extract_streams_wad_fp(ns_en_offsets, d_wad_fp, adb_data, extract_path, 'Dialogues, Vocals')
